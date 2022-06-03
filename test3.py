b4475f12d1a2ca16ac3646cf6ea23cd45a9fdd67
Denis-Bez <v417459@yandex.ru>
2022-06-04 00:21:51 +0500

    Add 'calculate' function

--- test3.py
+++ test3.py
@@ -1,6 +1,17 @@
-x = 4
-y = 5
 
-x = x - y
+def calculate(x, y):
+    x = x - y
 
-print(x)
+    return x
+
+def main():
+    x = 4
+    y = 5
+
+    x = calculate(x, y)
+
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
