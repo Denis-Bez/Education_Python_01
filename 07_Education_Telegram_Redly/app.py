870819f9bda47356c88f731d2beb6f002da7d07f
Denis-Bez <v417459@yandex.ru>
2022-06-22 01:59:59 +0500

    Telegram bot Education

---
+++ 07_Education_Telegram_Redly/app.py
@@ -0,0 +1,2 @@
+async def on_startup(dp):
+    print('Бот запущен')
---
+++ 07_Education_Telegram_Redly/utils/notify_admins.py
@@ -0,0 +1,17 @@
+# Logging facility
+import logging
+
+from aiogram import Dispatcher
+
+from data.config import admin_id
+
+
+async def on_startup_notify(dp: Dispatcher):
+    
+    for admin in admin_id:    
+        try: 
+            text = 'Бот запущен'
+            await dp.bot.send_message(chat_id=admin, text=text)
+        except Exeption as err:
+               logging.exception(err)
+
