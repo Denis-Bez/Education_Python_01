100cf2eab7f62b60ac5f21c8b2dd39c1865a63b9
Denis-Bez <v417459@yandex.ru>
2022-06-26 22:34:40 +0500

    Flask Education

--- 05_Education_Flask/FDataBase.py
+++ 05_Education_Flask/FDataBase.py
@@ -2,6 +2,8 @@ import sqlite3
 import time
 import math
 
+from flask import url_for
+
 
 class FDataBase:
     def __init__(self, db):
@@ -44,9 +46,19 @@ class FDataBase:
     # Get post from table "posts"
     def getPost(self, alias):
         try:
-            self.__cur.execute(f'SELECT title, text FROM posts WHERE url = "{alias}" LIMIT 1')
+            self.__cur.execute(f'SELECT title, text FROM posts WHERE url LIKE "{alias}" LIMIT 1')
             res = self.__cur.fetchone() # Mehod takes one value
             if res:
+                
+                # Regular expressions to invite full links to image
+                # !!!Better save correcting link to data base for saving resourses
+                # base = url_for('static', filename='img')
+                # url to image
+                # text = re.sub(r'(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>',
+                #                 '\\g<tag>' + base + "/\\g<url>>",
+                #                 res['text'])
+                # return (res['tittle', text])
+
                 return res
         except sqlite3.Error as e:
             print("Ошибка получения статьи из БД"+str(e))
--- 05_Education_Flask/app.py
+++ 05_Education_Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash, url_for, session, request, abort, g
+from flask import Flask, make_response, render_template, redirect, flash, url_for, session, request, abort, g, make_response
 from config import CONFIG
 import sqlite3
 import os
@@ -81,6 +81,72 @@ def showPost(alias):
         
         return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
+
+# Generate a server response
+#@application.route('/lesson_11')
+#def lesson_11():
+
+    # # Opportunity using 'make_response' №1
+    # content = render_template('index.html', posts=[])
+    # # make_response(res_body, status_code=200), where 'res_body' - sending content, 'satatus_code' - server response code
+    # res = make_response(content)
+    # # display 'index.html' as a regular string
+    # res.headers['Content-Type'] = 'text/plain'
+    # # optional
+    # res.headers['Server'] = 'flasksite'
+    # return res
+
+    # Opportunity using 'make_response' №2
+    # img = None
+    # with application.open_resource(application.root_path + '\static\img\prince1.jpg', mode='rb') as f:
+    #     img = f.read()
+    # if img is None:
+    #     return 'None image'   
+    # res = make_response(img)
+    # res.headers['Content-Type'] = 'image/jpg' # If write 'text/plain' that browser doesn't show picture
+    # return res
+
+    # # Opportunity using 'make_response' №3
+    # res = make_response('<h1>Ошибка сервера<h1>', 500)
+    # return res
+
+    # Three types server response 1) (response, status, headers) 2) (response, headers) 3) (response, status)
+    # @application.errorhandler(404)
+    # def pageNot(error):
+    #     return ('Страница не найдена', 404)
+    # return '<h1>Main Page</h1>', 200, {'Content-Type': 'text/plan'}
+
+    # 302 (temporarily), 301 (permanent) Redurect. Function in Flsk: redirect(location, status)
+    # return redirect(url_for('index'), 301)
+
+    # hooking of requests (перехват запросов)
+# @application.before_first_request
+# def before_first_request():
+#     print('before_first_request() called')
+# @application.before_request
+# def before_request():
+#     print('before_request() called')
+# @application.after_request
+# def after_request(response):
+#     print('after_request() called')
+#     return response
+# @application.teardown_request
+# def teardown_request(response):
+#     print('teardown_request() called')
+#     return response
+
+# Set cookies. function - set_cookie(key, value='', max_age=None(in seconds))
+@application.route('/login')
+def login():
+    log = ''
+    if request.cookies.get('logged'):
+        log = request.cookies.get('logged')
+    
+    res = make_response(f'<h1>Форма авторизации</h1><p>logged: {log}')
+    # Record information 'yes' on key 'logged'
+    res.set_cookie('logged', 'yes')
+    # return response with cookie
+    return res
 
 if __name__ == "__main__":
     application.run(debug=True)
