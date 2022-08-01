22007322bad182092e49bf3d16d8fcfef18f042e
Denis-Bez <v417459@yandex.ru>
2022-08-02 01:50:26 +0500

    Task 'Search_Celebrate'

--- 09_Education_Technical_Interviews/02_Task_Search_Celebrity.py
+++ 09_Education_Technical_Interviews/02_Task_Search_Celebrity.py
@@ -2,10 +2,10 @@
 # Task analysis: https://www.youtube.com/watch?v=xGvQN_g-JCI
 
 # We have 'k' people. We can ask every human, "would do you know that human?" and had get answer - 'not' or 'yes'.
-# Need to find celebrity, who don't now everybody, but everybody knows him
+# Need to find a celebrity, which doesn't know enyone, but enyone knows him
 
 """
-
+l                   k-1  (people index)
 \O/  \O/    \O/     \O/
  |    |      |       |
 / \  / \    / \     / \ 
@@ -15,3 +15,24 @@
              <-------4
 
 """
+
+def search_celebrity(person):
+    k = len(person)
+    # person1.knows(person2) - method return True if 'person1' knows 'person2' and False if 'person1' doesn't know 'person2'
+
+    l = 0
+    r = k-1
+
+    while not l==r:
+        if people(l).knows(people(r)):
+            l+= 1
+        else:
+            r-= 1
+
+    for i in range(0, k-1):
+        if (not i==l) and ( (people(l).knows(people(i))) or (not people(i).knows(people(l)) ) ):
+            return False
+        else:
+            return person[l]
+
+    # O = k + 2k = O(k)
