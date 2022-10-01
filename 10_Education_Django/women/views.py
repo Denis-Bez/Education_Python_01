f04770ece831f2f9f4c8017b1701275834e03816
Denis-Bez <v417459@yandex.ru>
2022-10-02 02:36:07 +0500

    Djangp education

--- 10_Education_Django/women/views.py
+++ 10_Education_Django/women/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 # request (ссылка на класс HttpRequest) содержит информацию о сессиях о куках и т.д.
@@ -7,8 +7,17 @@ def index(request):
     return HttpResponse('Страница приложения women.')
 
 def categories(request, catid):
-    print(request.GET)
+    if (request.GET):
+        print(request.GET) # Demonstration of extraction Get-parametrs or POST-paramentrs
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
 
 def archive(request, year):
+    if int(year) > 2020 and int(year) < 2025:
+        return redirect('home') # 302-redirect
+    if int(year) > 2025:
+        return redirect('home', permanent=True) # 301-redirect
+
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
