3b941a3140cbe28765afccaa21913c4a8b753846
Denis-Bez <v417459@yandex.ru>
2022-10-08 18:26:22 +0500

    Django Education

--- 10_Education_Django/coolsite/settings.py
+++ 10_Education_Django/coolsite/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Shared folder on real server
-# STATICFILES_DIRS = [] # List of atypical paths
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Shared folder on real server
+STATICFILES_DIRS = [] # List of atypical paths
 
 
 # Default primary key field type
