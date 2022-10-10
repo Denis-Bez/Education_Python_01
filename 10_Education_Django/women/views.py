6abe8bde729a031564000cb8f8bf00bb025cc05c
Denis-Bez <v417459@yandex.ru>
2022-10-11 02:25:35 +0500

    Education Django

--- 10_Education_Django/women/views.py
+++ 10_Education_Django/women/views.py
@@ -1,29 +1,51 @@
+from calendar import c
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from .models import *
-menu = ['About', 'Add article', 'Feedback', 'Sing in']
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 # request (ссылка на класс HttpRequest) содержит информацию о сессиях о куках и т.д.
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Main Page'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context) # 'context' is special named parametr
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About site'})
 
-def categories(request, catid):
-    if (request.GET):
-        print(request.GET) # Demonstration of extraction Get-parametrs or POST-paramentrs
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
-def archive(request, year):
-    if int(year) > 2020 and int(year) < 2025:
-        return redirect('home') # 302-redirect
-    if int(year) > 2025:
-        return redirect('home', permanent=True) # 301-redirect
+def contact(request):
+    return HttpResponse("Обратная связь")
 
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+# def categories(request, catid):
+#     if (request.GET):
+#         print(request.GET) # Demonstration of extraction Get-parametrs or POST-paramentrs
+#     return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+
+# def archive(request, year):
+#     if int(year) > 2020 and int(year) < 2025:
+#         return redirect('home') # 302-redirect
+#     if int(year) > 2025:
+#         return redirect('home', permanent=True) # 301-redirect
+#     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
