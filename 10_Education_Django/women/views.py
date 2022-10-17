0e4983f76a8f7f55f60fd855e25284eeaaca9567
Denis-Bez <v417459@yandex.ru>
2022-10-18 01:17:46 +0500

    Django Education

--- 10_Education_Django/women/views.py
+++ 10_Education_Django/women/views.py
@@ -13,10 +13,14 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 # request (ссылка на класс HttpRequest) содержит информацию о сессиях о куках и т.д.
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context) # 'context' is special named parametr
 
@@ -34,6 +38,23 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'View Categories',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 # def categories(request, catid):
 #     if (request.GET):
