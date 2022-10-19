643c1f0916041fe3ba44e169c6d4a76e7c0bf58a
Denis-Bez <v417459@yandex.ru>
2022-10-20 03:13:03 +0500

    Bot Education

---
+++ ED11_Python-Telegram-Bot/handlers/handlers.py
@@ -0,0 +1,40 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):    
+    # First message (echo)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    # Second message (you say...)
+    await update.message.reply_text("You said: " + update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='CAPS',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
---
+++ ED11_Python-Telegram-Bot/test.py
@@ -0,0 +1,11 @@
+a = 3
+
+def a():
+    a = 2
+
+def b(A: a):
+    print(A + 2)
+
+
+if __name__ == '__main__':
+    b()
