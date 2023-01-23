d95e112b3ff4e0420e5f88d7fb78bc0861fba826
Denis-Bez <v417459@yandex.ru>
2023-01-23 22:13:50 +0500

    Education Django lesson 13

--- 10_Education_Django/coolsite/settings.py
+++ 10_Education_Django/coolsite/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us' # Russian: 'ru'
+LANGUAGE_CODE = 'ru' # Russian: 'ru' English: en-us
 
 TIME_ZONE = 'UTC'
 
--- 10_Education_Django/women/views.py
+++ 10_Education_Django/women/views.py
@@ -2,6 +2,7 @@ from calendar import c
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .forms import AddPostForm
 from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -9,6 +10,7 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
 ]
+
 
 # request (ссылка на класс HttpRequest) содержит информацию о сессиях о куках и т.д.
 def index(request):
@@ -22,17 +24,36 @@ def index(request):
     }
     return render(request, 'women/index.html', context=context) # 'context' is special named parametr
 
+
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About site'})
 
+
 def addpage(request):
-    return HttpResponse("Добавление статьи")
+    
+    # Don't clear fields if was error
+    if request.method == 'POST':
+        form = AddPostForm(request.POST) # Dictionary with filled in field
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data) # Add to database
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добваления поста')
+    else:
+        form = AddPostForm()
+
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+
 
 def contact(request):
     return HttpResponse("Обратная связь")
 
+
 def login(request):
     return HttpResponse("Авторизация")
+
 
 def show_post(request, post_slug):
     post = get_object_or_404(Women, slug=post_slug)
