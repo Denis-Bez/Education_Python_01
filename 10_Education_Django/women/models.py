0f96c049f3de23fc2b275edce97c1a95b2ce3f52
Denis-Bez <v417459@yandex.ru>
2023-02-01 15:52:57 +0500

    Education Django Lesson 16

--- 10_Education_Django/women/models.py
+++ 10_Education_Django/women/models.py
@@ -11,7 +11,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)  # If add - related_name='get_posts', then we can use not "c.women_set.all()" but "c.get_posts.all()"
 
     def __str__(self):
         return self.title
