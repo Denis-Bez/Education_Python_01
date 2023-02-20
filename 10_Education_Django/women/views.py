e027c5ab8adb87ccd21c2ca699a6844a78827394
Denis-Bez <v417459@yandex.ru>
2023-02-20 23:31:08 +0500

    Django Education Lesson 19 (Chapter 2)

--- 10_Education_Django/women/views.py
+++ 10_Education_Django/women/views.py
@@ -8,6 +8,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.contrib.auth.forms import UserCreationForm
 
 from .forms import AddPostForm
 from .models import *
@@ -176,4 +177,11 @@ def pageNotFound(request, exception):
 
 
 class RegisterUser(DataMixin, CreateView):
-    pass
+    form_class = UserCreationForm # Standart Django form for users registration
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list( context.items() + list(c_def.items())))
