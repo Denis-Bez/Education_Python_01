ccfa0188e722b77e0c46a1e1b2058513aeec92b1
Denis Bezvershenko <v417459@yandex.ru>
2023-03-29 11:57:47 +0500

    Fix misspel

--- 10_Education_Django/coolsite/urls.py
+++ 10_Education_Django/coolsite/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from coolsite import settings
 from women.views import *
-from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
